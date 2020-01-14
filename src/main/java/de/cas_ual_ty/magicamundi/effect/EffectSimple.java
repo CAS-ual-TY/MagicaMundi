fd64f5363364c1d2c3e2d745fb8e299c30e9e48d
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-01-14 19:25:27 +0000

    Simple implementations of effect and sorter

---
+++ src/main/java/de/cas_ual_ty/magicamundi/effect/EffectSimple.java
@@ -0,0 +1,28 @@
+package de.cas_ual_ty.magicamundi.effect;
+
+import java.util.List;
+
+import de.cas_ual_ty.magicamundi.target.Target;
+
+public abstract class EffectSimple extends Effect
+{
+    public EffectSimple()
+    {
+    }
+    
+    @Override
+    public boolean applyEffect(List<Target> list)
+    {
+        for(Target t : list)
+        {
+            if(!this.applyEffect(t))
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public abstract boolean applyEffect(Target target);
+}
---
+++ src/main/java/de/cas_ual_ty/magicamundi/sorter/SorterSimple.java
@@ -0,0 +1,33 @@
+package de.cas_ual_ty.magicamundi.sorter;
+
+import java.util.List;
+
+import de.cas_ual_ty.magicamundi.target.Target;
+
+public abstract class SorterSimple extends Sorter
+{
+    public SorterSimple()
+    {
+    }
+    
+    @Override
+    public boolean sortOut(List<Target> list1, List<Target> list2)
+    {
+        for(Target t : list1)
+        {
+            if(!this.keep(t))
+            {
+                list2.add(t);
+            }
+        }
+        
+        for(Target t : list2)
+        {
+            list1.remove(t);
+        }
+        
+        return true;
+    }
+    
+    public abstract boolean keep(Target target);
+}
