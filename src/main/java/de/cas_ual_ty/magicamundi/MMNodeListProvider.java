513f64ffba813644dda2fe6b1ffde178cd5e89be
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-01-30 13:44:14 +0100

    Created MM node list provider

---
+++ src/main/java/de/cas_ual_ty/magicamundi/MMNodeListProvider.java
@@ -0,0 +1,27 @@
+package de.cas_ual_ty.magicamundi;
+
+import java.util.ArrayList;
+
+import de.cas_ual_ty.magicamundi.registries.MMNodeTypes;
+import de.cas_ual_ty.visibilis.node.Node;
+import de.cas_ual_ty.visibilis.print.NodeListProviderBase;
+
+public class MMNodeListProvider extends NodeListProviderBase
+{
+    public MMNodeListProvider()
+    {
+        super();
+        MMNodeListProvider.mmNodes(this.list);
+    }
+    
+    public static void mmNodes(ArrayList<Node> list)
+    {
+        list.add(MMNodeTypes.SELEKTOR_SENDER.instantiate());
+        
+        list.add(MMNodeTypes.SORTER_ENTITY.instantiate());
+        list.add(MMNodeTypes.SORTER_LIVING_ENTITY.instantiate());
+        list.add(MMNodeTypes.SORTER_PLAYER.instantiate());
+        
+        list.add(MMNodeTypes.EFFECT_FIRE.instantiate());
+    }
+}
